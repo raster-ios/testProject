a62c3f5e5ecb80279ed63b58bf6b23a16a057cad
Gennady Raster <raster.iosdev@gmail.com>
2022-01-21 16:59:25 +0300

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Gena Raster on 21.01.22.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
